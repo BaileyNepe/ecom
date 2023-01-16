b403c509bc6e950461a537f74c400bf9a6f96219
Bailey Nepe <bailey.nepe@numberworks.com>
2023-01-17 08:51:39 +1300

    tets

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-zinc-800 container">
+      <h1 className="text-3xl font-bold underline">Header</h1>
+    </header>
+  );
+};
+
+export default Header;
--- client/src/layouts/basic/index.tsx
+++ client/src/layouts/basic/index.tsx
@@ -1,16 +1,12 @@
 import { Outlet } from 'react-router';
+import Header from '../../components/Header';
 import Loader from '../../components/Loader';
 
 const BasicLayout = () => {
   return (
     <>
-      <header></header>
-      <main
-        // make background color black
-        className="bg-black"
-      >
-        <div>ggg</div>
-        <Loader />
+      <Header />
+      <main>
         <Outlet />
       </main>
       <footer> </footer>
